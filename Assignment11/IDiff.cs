63dbb836ca594fde0b577bde43a04584ea55eaf9
invernoe <mahmoudk221999@gmail.com>
2024-07-25 14:59:34 +0300

    Add project files.

---
+++ Assignment11/IDiff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal interface IDiff<T> where T : IDiff<T>
+    {
+        static abstract T operator -(T left, T right);
+    }
+}
